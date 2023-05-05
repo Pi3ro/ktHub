42da2b7edcee2349dc0a231def6567490352888e
Piero <spieropc@gmail.com>
2023-05-05 11:59:56 -0500

    [/] HotbarListener

--- src/main/kotlin/me/pi3ro/hub/hotbar/HotbarListener.kt
+++ src/main/kotlin/me/pi3ro/hub/hotbar/HotbarListener.kt
@@ -17,32 +17,29 @@ import org.bukkit.inventory.ItemStack
  * Date: 2023-05-04 @ 20:19
  **/
 class HotbarListener : Listener {
-
     @EventHandler
-    fun onEnderButt(event: PlayerInteractEvent) {
-        if (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK) {
-            val player: Player = event.player
-            if (player.itemInHand.type == Material.ENDER_PEARL) {
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        val player = event.player
+        val item = player.itemInHand
+
+        if (event.action != Action.RIGHT_CLICK_AIR && event.action != Action.RIGHT_CLICK_BLOCK) {
+            return
+        }
+
+        when (item.type) {
+            Material.ENDER_PEARL -> {
                 event.isCancelled = true
                 event.setUseInteractedBlock(Event.Result.DENY)
                 event.setUseItemInHand(Event.Result.DENY)
                 player.velocity = player.location.direction.normalize().multiply(2.5)
                 player.updateInventory()
             }
-        }
-    }
-
-    @EventHandler
-    fun onInteract(event: PlayerInteractEvent){
-        val player: Player = event.player
-        if (event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK){
-            val itemStack: ItemStack? = event.item
-            if (itemStack == null || itemStack.type == Material.AIR) return;
-            if (!itemStack.hasItemMeta() || !itemStack.itemMeta.hasDisplayName()) return;
-
-            if (itemStack.type == Material.WATCH && itemStack.itemMeta.displayName.equals(CC.translate("&bServer Selector &7(Right Click)"))){
-                ServerSelectorMenu().openMenu(player)
+            Material.WATCH -> {
+                if (item.hasItemMeta() && item.itemMeta.hasDisplayName() && item.itemMeta.displayName.equals(CC.translate("&bServer Selector &7(Right Click)"), ignoreCase = true)) {
+                    ServerSelectorMenu().openMenu(player)
+                }
             }
+            else -> return
         }
     }
 }
